be5135afa2be5f3507db83917299f195ad958f57
yonatanvolsky <yonatanes1@gmail.com>
2023-11-15 10:59:51 +0200

    Added decrypt code and decryptGUI

--- src/Logics/decrypt.java
+++ src/Logics/decrypt.java
@@ -2,39 +2,49 @@ package Logics;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
-import java.io.*;
+import java.awt.font.GlyphMetrics;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
-import java.util.Base64;
 
 
 public class decrypt {
-
-    public static decryptFile(String filename) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException {
-
+    /**
+     * Runner of the decryption process
+     * @param filename absolute path of the encrypted file
+     */
+    public static void decryptFile(String filename) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException {
+        String destName = filename.replace("Enc.txt", "Dec.txt");
+        SecretKey key = Global.getInstance().aesData.getKey();
+        IvParameterSpec iv = Global.getInstance().aesData.getIv();
         File src = new File(filename);
-        String destName = "aaaa.txtenc";
         File dest = new File(destName);
         FileInputStream inputStream = new FileInputStream(src);
         FileOutputStream outputStream = new FileOutputStream(dest);
+
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
 
         byte[] buffer = new byte[64];
         int bytesRead;
 
         while ((bytesRead = inputStream.read(buffer)) != -1) {
-            byte[] output =
+            byte[] output = cipher.update(buffer, 0, bytesRead);
             if (output != null) {
                 outputStream.write(output);
             }
         }
 
-
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
     }
-
-
-
-
 }
-
