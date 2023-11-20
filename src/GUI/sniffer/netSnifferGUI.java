30e7e1553ffac5fd20c1412d0454263e8feb1a0a
galpearl <galpearl24@gmail.com>
2023-11-20 19:13:16 +0200

    Port filtering finished, code organization.

--- src/GUI/sniffer/netSnifferGUI.java
+++ src/GUI/sniffer/netSnifferGUI.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
 
 /**
@@ -26,8 +27,8 @@ public class netSnifferGUI {
     JRadioButton HTTPrad = new JRadioButton("HTTP (80)");
     JRadioButton POP3rad = new JRadioButton("POP3 (110)");
     JRadioButton IMAPrad = new JRadioButton("IMAP (143)");
-    JRadioButton SNMPrad = new JRadioButton("SNMP (161");
-    JRadioButton LDAPrad = new JRadioButton("LDAP (389");
+    JRadioButton SNMPrad = new JRadioButton("SNMP (161)");
+    JRadioButton LDAPrad = new JRadioButton("LDAP (389)");
     JRadioButton HTTPSrad = new JRadioButton("HTTPS (443)");
     JRadioButton RDPrad = new JRadioButton("RDP (3389)");
     ButtonGroup ports = new ButtonGroup();
@@ -41,14 +42,17 @@ public class netSnifferGUI {
         this.controlPanel = new JPanel();
         this.portsPanel = new JPanel();
         this.buttonsPanel = new JPanel();
-        normalPorts = new JPanel();
+        this.normalPorts = new JPanel();
         this.frame = frame;
 
         frame.setTitle("SwissJnife - Network Traffic");
-
         this.frame.setSize(860, 650);
         this.frame.setLocation(sharedUtils.centerFrame(this.frame));
+
         mainPanel.setLayout(new BoxLayout(mainPanel,BoxLayout.Y_AXIS));
+        portsPanel.setLayout(new BoxLayout(portsPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBackground(Color.WHITE);
+        controlPanel.setBackground(Color.WHITE);
         this.frame.add(mainPanel);
 
     }
@@ -73,6 +77,11 @@ public class netSnifferGUI {
         mainPanel.add(controlPanel);
     }
 
+    /**
+     * Sets the text pane for scrolling
+     * @param log log to attach the pane
+     * @return whole panning area
+     */
     @NotNull
     private static JScrollPane setLogPane(JTextArea log) {
         JScrollPane sp = new JScrollPane();
@@ -84,6 +93,10 @@ public class netSnifferGUI {
         return sp;
     }
 
+    /**
+     * Create the textual log
+     * @return log window
+     */
     @NotNull
     private static JTextArea createLogArea() {
         JTextArea log = new JTextArea();
@@ -98,68 +111,126 @@ public class netSnifferGUI {
         log.setCursor(new Cursor(Cursor.TEXT_CURSOR));
         return log;
     }
+
+    /**
+     * Adding the controlling panels
+     */
     public void addPanels() {
-
         controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.X_AXIS));
-
         setPortsPanel();
+        setButtonsPanel();
+
         controlPanel.add(portsPanel);
         controlPanel.add(Box.createRigidArea(new Dimension(5,0)));
-        setButtonsPanel();
         controlPanel.add(buttonsPanel);
     }
+
+    /**
+     * Creates the port filtering options control panel
+     */
     public void setPortsPanel() {
+        Component rigidArea = Box.createRigidArea(new Dimension(0, 10));
+        rigidArea.setBackground(new Color(189,189,189));
+
+        JLabel title = new JLabel("Port Filter");
+        title.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+        portsPanel.add(title);
+
         portsPanel.setBackground(new Color(189, 189, 189));
         portsPanel.setBorder(BorderFactory.createEtchedBorder());
-        JPanel portCol1 = new JPanel();
-        portCol1.setLayout(new BoxLayout(portCol1, BoxLayout.Y_AXIS));
-        portCol1.setBackground(new Color(189, 189, 189));
-        portSetup(FTPrad);
-        portSetup(SSHrad);
-        portSetup(TELNETrad);
-        portSetup(SMTPrad);
-        portCol1.add(FTPrad);
-        portCol1.add(SSHrad);
-        portCol1.add(TELNETrad);
-        portCol1.add(SMTPrad);
-        normalPorts.add(portCol1);
+        portsPanel.setMaximumSize(new Dimension(250, 165));
+        portsPanel.setMinimumSize(new Dimension(250, 165));
 
-        JPanel portCol2 = new JPanel();
-        portCol2.setLayout(new BoxLayout(portCol2, BoxLayout.Y_AXIS));
-        portCol2.setBackground(new Color(189, 189, 189));
-        portSetup(DNSrad);
-        portSetup(HTTPrad);
-        portSetup(POP3rad);
-        portSetup(IMAPrad);
-        portCol2.add(DNSrad);
-        portCol2.add(HTTPrad);
-        portCol2.add(POP3rad);
-        portCol2.add(IMAPrad);
-        normalPorts.add(portCol2);
+        colSetup(FTPrad, SSHrad, TELNETrad, SMTPrad);
+        colSetup(DNSrad, HTTPrad, POP3rad, IMAPrad);
+        colSetup(SNMPrad, LDAPrad, HTTPSrad, RDPrad);
 
-        JPanel portCol3 = new JPanel();
-        portCol3.setLayout(new BoxLayout(portCol3, BoxLayout.Y_AXIS));
-        portCol3.setBackground(new Color(189, 189, 189));
-        portSetup(SNMPrad);
-        portSetup(LDAPrad);
-        portSetup(HTTPSrad);
-        portSetup(RDPrad);
-        portCol3.add(SNMPrad);
-        portCol3.add(LDAPrad);
-        portCol3.add(HTTPSrad);
-        portCol3.add(RDPrad);
-        normalPorts.add(portCol3);
-
+        portsPanel.add(rigidArea);
         portsPanel.add(normalPorts);
+
+        JPanel misc = customPortPanel();
+
+        portsPanel.add(rigidArea);
+        portsPanel.add(misc);
     }
+
+    /**
+     * Setting each column of JRadioButtons
+     * @param btnOne first button
+     * @param btnTwo second button
+     * @param btnThree third button
+     * @param btnFour fourth button
+     */
+    private void colSetup(JRadioButton btnOne, JRadioButton btnTwo, JRadioButton btnThree, JRadioButton btnFour) {
+        JPanel portCol1 = getColPanel();
+        portSetup(btnOne);
+        portSetup(btnTwo);
+        portSetup(btnThree);
+        portSetup(btnFour);
+        portCol1.add(btnOne);
+        portCol1.add(btnTwo);
+        portCol1.add(btnThree);
+        portCol1.add(btnFour);
+        normalPorts.add(portCol1);
+    }
+
+    @NotNull
+    private static JPanel getColPanel() {
+        JPanel col = new JPanel();
+        col.setLayout(new BoxLayout(col, BoxLayout.Y_AXIS));
+        col.setBackground(new Color(189, 189, 189));
+        return col;
+    }
+
+    /**
+     * Sets the custom ports panel filtering
+     * @return panel for filtering custom ports
+     */
+    @NotNull
+    private static JPanel customPortPanel() {
+        JPanel misc = new JPanel();
+        misc.setLayout(new BoxLayout(misc, BoxLayout.X_AXIS));
+        misc.setBackground(new Color(189, 189, 189));
+
+        JLabel customPortLabel = new JLabel("Custom Port: ");
+        customPortLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+        misc.add(customPortLabel);
+
+        JTextField customPort = new JTextField();
+        customPort.setBackground(Color.WHITE);
+        customPort.setBorder(new EtchedBorder());
+        customPort.setMaximumSize(new Dimension(60, 22));
+        customPort.setMinimumSize(new Dimension(60, 22));
+        customPort.setHorizontalAlignment(JTextField.CENTER);
+        misc.add(customPort);
+
+        JButton addPortBtn = new JButton("Listen");
+        addPortBtn.setBackground(Color.GREEN);
+        addPortBtn.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        addPortBtn.setMaximumSize(new Dimension(69, 21));
+        addPortBtn.setMaximumSize(new Dimension(69, 21));
+        sharedUtils.noFocusBorder(addPortBtn);
+        addPortBtn.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        misc.add(addPortBtn);
+        return misc;
+    }
+
+    /**
+     * Creates general controls panel for the analyzer.
+     */
     public void setButtonsPanel() {
         buttonsPanel.setBackground(new Color(189, 189, 189));
         buttonsPanel.setBorder(BorderFactory.createEtchedBorder());
         buttonsPanel.setPreferredSize(new Dimension(300, 165));
-        normalPorts = new JPanel();
         normalPorts.setBackground(new Color(189, 189, 189));
         normalPorts.setLayout(new BoxLayout(normalPorts, BoxLayout.X_AXIS));
     }
+
+    /**
+     * Setting visual attributes to a button
+     * @param btn button to set
+     */
     public void portSetup(JRadioButton btn) {
         btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
         btn.setFont(new Font("Tahoma", Font.PLAIN, 11));
